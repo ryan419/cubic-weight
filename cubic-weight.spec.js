6eef25b6e4bc4a5b080c3ecf8f5faddb362ba535
Ryan Xiao <ryan.shaw.0419@google.com>
2018-12-19 00:33:46 +1100

    add unit test

---
+++ cubic-weight.spec.js
@@ -0,0 +1,46 @@
+const assert = require('assert');
+const getAverageCubicWeight = require('./cubic-weight');
+
+const testItems = [{
+    category: 'Air Conditioners',
+    title: 'Window Seal for Portable Air Conditioner Outlets',
+    weight: 235,
+    size: {
+      width: 100,
+      length: 100,
+      height: 100
+    }
+  },
+  {
+    category: 'Air Conditioners',
+    title: 'Kogan 10,000 BTU Portable Air Conditioner (2.9KW)',
+    weight: 26200,
+    size: {
+      width: 100,
+      length: 100,
+      height: 100
+    }
+  }
+];
+
+const testItems2 = [{
+  category: 'Air Conditioners',
+  title: 'Window Seal for Portable Air Conditioner Outlets',
+  weight: 235,
+  size: {
+    width: 30,
+    length: 40,
+    height: 20
+  }
+}];
+
+describe('Avarage cubic weight', function() {
+  describe('Calc avarage cubic weight', function() {
+    it('average cubic weight should be 250 kg when length, height, width are 1 meter', function() {
+      assert.equal(getAverageCubicWeight(testItems), 250);
+    });
+    it('average cubic weight should be 6 kg when long (0.4m) x 20cm high (0.2m) x 30cm wide (0.3m)', function() {
+      assert.equal(getAverageCubicWeight(testItems2), 6);
+    });
+  });
+});
