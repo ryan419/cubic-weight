b242f993f4dc4271044ac28d0ee091705631ef36
Ryan Xiao <ryan.shaw.0419@google.com>
2018-12-19 00:17:25 +1100

    calc cubic weight

---
+++ cubic-weight.js
@@ -0,0 +1,14 @@
+const Decimal = require('decimal.js');
+
+const conversionFactor = 250;
+const cmToM = (length) => new Decimal(length).div(100);
+const getVolume = ({ width, length, height }) => cmToM(width)
+  .mul(cmToM(length))
+  .mul(cmToM(height));
+const getCubicWeight = (item) => getVolume(item.size).mul(conversionFactor);
+const getTotalCubicWeight = items => items.reduce((total, item) => total.add(getCubicWeight(item)), new Decimal(0));
+const getAverageCubicWeight = (items) => getTotalCubicWeight(items)
+  .div(items.length)
+  .toNumber();
+
+module.exports = getAverageCubicWeight;
---
+++ index.js
@@ -0,0 +1,24 @@
+const axios = require('axios');
+const { empty, from } = require('rxjs');
+const { expand, map, reduce, takeWhile, tap } = require('rxjs/operators');
+const getAverageCubicWeight = require('./cubic-weight')
+
+const baseUrl = 'http://wp8m3he1wt.s3-website-ap-southeast-2.amazonaws.com';
+
+const getProducts = (endPoint) => from(axios.get(`${baseUrl}${endPoint}`))
+  .pipe(map(res => res.data));
+
+const filterAirConditioner = (items) => items.filter(item => item.category === 'Air Conditioners');
+
+getProducts('/api/products/1')
+  .pipe(
+    expand(({ next }) => next ? getProducts(next) : empty()),
+    takeWhile(data => !!data),
+    map(({ objects }) => objects),
+    reduce((res, items) => ([...(res || []), ...items])),
+    map(filterAirConditioner)
+  )
+  .subscribe(itemList => {
+    const avg = getAverageCubicWeight(itemList);
+    console.log(`Average Cubic Weight: ${avg} kg`);
+  });
